5515ec21eece1f6d7ae54a83ea34610613507d15
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2022-12-14 23:10:56 +0100

    modified Dashboard.js and index.css

--- Client/src/Pages/Dashboard.js
+++ Client/src/Pages/Dashboard.js
@@ -11,27 +11,24 @@ function Dashboard() {
       <Sidebar />
       <section className="fixed h-screen left-64 display-size bg-gray-200 dark:bg-gray-800 p-9">
         <main>
-          <div className="w-full lg:w-2/3 py-3 flex justify-between items-center shadow-sm">
+          <div className="w-full xl:w-2/3 py-3 flex justify-between items-center shadow-sm">
             <h2 className="font-raleway font-bold text-2xl">Dashboard</h2>
             <div className="flex items-center justify-center space-x-5">
-              <i class="fas fa-bars fa-lg fa-fw"></i>
-              <div>
-                <div>
-                  <div class="flex rounded-md shadow-sm">
-                    <span class="p-3 flex items-center min-w-fit rounded-l-md border border-r-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:border-gray-700 dark:text-gray-400">
-                      Default
-                    </span>
-                    <input
-                      type="text"
-                      class="py-3 pr-11 block w-full border-gray-200 shadow-sm rounded-r-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
-                    />
-                  </div>
-                </div>
+              <div class="flex rounded-md shadow-sm font-raleway">
+                <button className="py-2 px-3 btn bg-white border-gray-200 shadow-sm rounded-l-md text-md dark:bg-gray-800 dark:text-gray-400 dark:border-l  dark:border-b dark:border-t dark:border-blueDeep">
+                  <i class="fas fa-search fa-sm fa-fw"></i>
+                </button>
+                <input
+                  type="text"
+                  class="py-2 pr-11 block w-full border-gray-200 shadow-sm rounded-r-md text-md focus:z-10 dark:focus:border-r dark:focus:border-b dark:focus:border-t focus:border-blueDeep dark:bg-gray-800 dark:text-gray-400 dark:border-r dark:border-b dark:border-t dark:border-blueDeep"
+                  placeholder="Search"
+                  autoComplete="true"
+                />
               </div>
             </div>
           </div>
-          <div className="row w-full lg:w-2/3 py-4 flex space-x-6 overflow-x-auto">
-            <div className="p-10 lg:py-8 lg:px-6 bg-gray-300 dark:bg-gray-800 dark:border dark:border-gray-700 shadow-lg rounded-lg">
+          <div className="row w-full xl:w-2/3 py-4 flex space-x-6 overflow-x-auto">
+            <div className="p-10 lg:py-10 lg:px-12 bg-gray-300 dark:bg-gray-800 dark:border dark:border-gray-700 shadow-lg rounded-lg">
               <p className="pb-3 dark:text-gray-400">
                 <span className="text-md font-light font-raleway">
                   Total Earning
@@ -48,8 +45,40 @@ function Dashboard() {
                 $450,000.30
               </p>
             </div>
-            <div className="py-16 px-20 bg-gray-300 rounded-lg"></div>
-            <div className="py-16 px-20 bg-gray-300 rounded-lg"></div>
+            <div className="p-10 lg:py-10 lg:px-12 bg-gray-300 dark:bg-gray-800 dark:border dark:border-gray-700 shadow-lg rounded-lg">
+              <p className="pb-3 dark:text-gray-400">
+                <span className="text-md font-light font-raleway">
+                  Total Earning
+                </span>
+                <i class="fas fa-chart-simple fa-lg fa-fw pl-4"></i>
+              </p>
+              <div class="w-full bg-gray-200 h-2 rounded-lg mb-3">
+                <div
+                  class="bg-blueDeep rounded-lg h-2"
+                  style={{ width: "70%" }}
+                ></div>
+              </div>
+              <p className="font-black font-archivo text-2xl dark:text-gray-400">
+                $450,000.30
+              </p>
+            </div>
+            <div className="p-10 lg:py-10 lg:px-12 bg-gray-300 dark:bg-gray-800 dark:border dark:border-gray-700 shadow-lg rounded-lg">
+              <p className="pb-3 dark:text-gray-400">
+                <span className="text-md font-light font-raleway">
+                  Total Earning
+                </span>
+                <i class="fas fa-chart-simple fa-lg fa-fw pl-4"></i>
+              </p>
+              <div class="w-full bg-gray-200 h-2 rounded-lg mb-3">
+                <div
+                  class="bg-blueDeep rounded-lg h-2"
+                  style={{ width: "70%" }}
+                ></div>
+              </div>
+              <p className="font-black font-archivo text-2xl dark:text-gray-400">
+                $450,000.30
+              </p>
+            </div>
           </div>
         </main>
       </section>

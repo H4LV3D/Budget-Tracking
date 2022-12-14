7451580f7ad9f370329f677c359ff0c75bc87874
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2022-12-14 02:27:35 +0100

    changed 2109 files, font awesome

--- Client/src/Pages/Dashboard.js
+++ Client/src/Pages/Dashboard.js
@@ -349,7 +349,7 @@ function Dashboard() {
               data-dropdown-toggle="language-dropdown"
               class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:hover:text-white dark:text-gray-400 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
             >
-              Proceed
+              <i class="fas fa-sign-out-alt fa-lg fa-fw"></i>
             </button>
 
             <div

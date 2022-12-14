226f6456902f6ae635523352bcf32ebcbcd7a0ba
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2022-12-14 03:54:37 +0100

    modified Sidebar.js and Dashboard.js

---
+++ Client/src/Components/Sidebar.js
@@ -0,0 +1,153 @@
+import React from "react";
+import Toggle from "./toggle";
+import "../Assets/fontawesome-free-6.2.1-web/css/solid.css";
+import "../Assets/fontawesome-free-6.2.1-web/css/all.css";
+import "../Assets/fontawesome-free-6.2.1-web/css/brands.css";
+import "../Assets/fontawesome-free-6.2.1-web/css/fontawesome.css";
+
+function Sidebar() {
+  return (
+    <>
+      <section>
+        <aside class="fixed top-0 left-0 w-64 h-full" aria-label="Sidenav">
+          <div class="overflow-y-auto py-5 px-3 h-full bg-gray-200 border-r border-gray-300 dark:bg-gray-800 dark:border-gray-700">
+            <a
+              href="/"
+              className="flex items-center md:mb-0 mr-3 mt-4 px-2 py-3"
+            >
+              <i className="fab fa-bitcoin fa-lg fa-fw text-blueDeep"></i>
+              <span className="text-blueDeep font-sans font-bold text-lg">
+                Coin
+              </span>
+              <span className="text-gray-600 dark:text-gray-400 font-raleway font-black text-lg">
+                Save
+              </span>
+            </a>
+            <ul class="space-y-2 mt-5">
+              <li>
+                <a
+                  href="/dashboard"
+                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
+                  <i class="fa-solid fa-house fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                  <span class="ml-3">Dashboard</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  type="button"
+                  class="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  aria-controls="dropdown-pages"
+                  data-collapse-toggle="dropdown-pages"
+                >
+                  <i class="fas fa-file-invoice-dollar fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                  <span class="flex-1 ml-3 text-left whitespace-nowrap">
+                    Invoice
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  type="button"
+                  class="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  aria-controls="dropdown-sales"
+                  data-collapse-toggle="dropdown-sales"
+                >
+                  <i class="fas fa-cart-shopping fa-lg fa-fw dark:text-white text-blueDeep"></i>
+                  <span class="flex-1 ml-3 text-left whitespace-nowrap">
+                    Sales
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
+                  <i class="fas fa-envelope fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                  <span class="flex-1 ml-3 whitespace-nowrap">Messages</span>
+                  <span class="inline-flex justify-center items-center w-5 h-5 text-xs font-semibold rounded-full text-primary-800 bg-primary-100 dark:bg-primary-200 dark:text-primary-800">
+                    6
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  type="button"
+                  class="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  <i class="fas fa-lock fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                  <span class="flex-1 ml-3 text-left whitespace-nowrap">
+                    Authentication
+                  </span>
+                </a>
+              </li>
+            </ul>
+            <ul class="pt-5 mt-5 space-y-2 border-t border-gray-400 dark:border-gray-700">
+              <li>
+                <a
+                  href="/"
+                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
+                >
+                  <i class="fas fa-file fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                  <span class="ml-3">Docs</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
+                >
+                  <i class="fas fa-folder fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                  <span class="ml-3">Components</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
+                >
+                  <i class="fa-solid fa-circle-info fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                  <span class="ml-3">Help</span>
+                </a>
+              </li>
+              <li>
+                <p class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                  <Toggle />
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div class="absolute bottom-0 lg:flex left-0 justify-center border-r bg-gray-200 border-gray-300 px-4 py-6 space-x-4 w-full dark:bg-gray-800 z-20">
+            <a
+              href="/"
+              class="inline-flex justify-center p-4 text-gray-500 rounded cursor-pointer dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600"
+            >
+              <i class="fas fa-globe-africa fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+            </a>
+            <a
+              href="/"
+              data-tooltip-target="tooltip-settings"
+              class="inline-flex justify-center p-4 text-gray-500 rounded cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
+            >
+              <i class="fas fa-gear fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+            </a>
+
+            <button
+              type="button"
+              data-dropdown-toggle="language-dropdown"
+              class="inline-flex justify-center p-4 text-gray-500 rounded cursor-pointer dark:hover:text-white dark:text-gray-400 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
+            >
+              <i class="fas fa-sign-out-alt fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+            </button>
+          </div>
+        </aside>
+      </section>
+    </>
+  );
+}
+
+export default Sidebar;
--- Client/src/Pages/Dashboard.js
+++ Client/src/Pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Toggle from "../Components/toggle";
+import Sidebar from "../Components/Sidebar";
 import "../Assets/fontawesome-free-6.2.1-web/css/solid.css";
 import "../Assets/fontawesome-free-6.2.1-web/css/all.css";
 import "../Assets/fontawesome-free-6.2.1-web/css/brands.css";
@@ -8,144 +8,37 @@ import "../Assets/fontawesome-free-6.2.1-web/css/fontawesome.css";
 function Dashboard() {
   return (
     <>
-      <section>
-        <aside class="fixed top-0 left-0 w-64 h-full" aria-label="Sidenav">
-          <div class="overflow-y-auto py-5 px-3 h-full bg-gray-200 border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-            <a
-              href="/"
-              className="flex items-center md:mb-0 mr-3 mt-4 px-2 py-3"
-            >
-              <i className="fab fa-bitcoin fa-lg fa-fw text-blueDeep"></i>
-              <span className="text-blueDeep font-sans font-bold text-lg">
-                Coin
-              </span>
-              <span className="text-gray-600 dark:text-gray-400 font-raleway font-black text-lg">
-                Save
-              </span>
-            </a>
-            <ul class="space-y-2 mt-5">
-              <li>
-                <a
-                  href="/"
-                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                >
-                  <i class="fa-solid fa-house fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span class="ml-3">Dashboard</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  type="button"
-                  class="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  aria-controls="dropdown-pages"
-                  data-collapse-toggle="dropdown-pages"
-                >
-                  <i class="fas fa-file-invoice-dollar fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span class="flex-1 ml-3 text-left whitespace-nowrap">
-                    Invoice
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  type="button"
-                  class="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  aria-controls="dropdown-sales"
-                  data-collapse-toggle="dropdown-sales"
-                >
-                  <i class="fas fa-cart-shopping fa-lg fa-fw dark:text-white text-blueDeep"></i>
-                  <span class="flex-1 ml-3 text-left whitespace-nowrap">
-                    Sales
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                >
-                  <i class="fas fa-envelope fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span class="flex-1 ml-3 whitespace-nowrap">Messages</span>
-                  <span class="inline-flex justify-center items-center w-5 h-5 text-xs font-semibold rounded-full text-primary-800 bg-primary-100 dark:bg-primary-200 dark:text-primary-800">
-                    6
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  type="button"
-                  class="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  <i class="fas fa-lock fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span class="flex-1 ml-3 text-left whitespace-nowrap">
-                    Authentication
-                  </span>
-                </a>
-              </li>
-            </ul>
-            <ul class="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
-              <li>
-                <a
-                  href="/"
-                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-                >
-                  <i class="fas fa-file fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span class="ml-3">Docs</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-                >
-                  <i class="fas fa-folder fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span class="ml-3">Components</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-                >
-                  <i class="fa-solid fa-circle-info fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span class="ml-3">Help</span>
-                </a>
-              </li>
-              <li>
-                <p class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-                  <Toggle />
-                </p>
-              </li>
-            </ul>
-          </div>
-          <div class="absolute bottom-0 lg:flex left-0 justify-center border-r bg-gray-200 border-gray-200 px-4 py-6 space-x-4 w-full dark:bg-gray-800 z-20">
-            <a
-              href="/"
-              class="inline-flex justify-center p-4 text-gray-500 rounded cursor-pointer dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600"
-            >
-              <i class="fas fa-globe-africa fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-            </a>
-            <a
-              href="/"
-              data-tooltip-target="tooltip-settings"
-              class="inline-flex justify-center p-4 text-gray-500 rounded cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
-            >
-              <i class="fas fa-gear fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-            </a>
-
-            <button
-              type="button"
-              data-dropdown-toggle="language-dropdown"
-              class="inline-flex justify-center p-4 text-gray-500 rounded cursor-pointer dark:hover:text-white dark:text-gray-400 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
-            >
-              <i class="fas fa-sign-out-alt fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-            </button>
-          </div>
-        </aside>
-      </section>
+      <Sidebar />
+      <div className="left-64 h-screen w-full">
+        <div
+          className={`left-64 flex items-center justify-center flex-col h-screen w-full`}
+        >
+          <section className="bg-gray-200 dark:bg-gray-800 w-full min-h-screen flex items-center  sm:my-0">
+            <div className="container mx-auto px-6 py-20 flex flex-col items-center justify-center">
+              <h2 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl text-center font-sans font-black text-blueDeep dark:text-white mb-8">
+                Follow <br /> <span className="text-blueDeep">The</span> Money!
+              </h2>
+              <p className="font-raleway text-center font-medium text-xl text-blueDeep dark:text-gray-200 mb-10">
+                Save. Spend. Invest. Repeat.
+              </p>
+              <p className="hidden text-grat-800 dark:text-gray-300 font-raleway font-light text-base text-center mx-auto w-full md:w-3/4 lg:w-1/2 my-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+                quaerat cumque unde hic porro dolores veritatis quos, doloribus
+                soluta nisi ea unde hic porro dolores veritatis.
+              </p>
+              <div className="flex flex-col md:flex-row  w-full px-10 justify-center">
+                <button className="bg-blueDeep text-center py-3 px-5 sm:px-10 mr-3 text-white text-lg rounded-lg mb-3 w-full md:w-[360px] hover:scale-110">
+                  Start Budgeting
+                </button>
+                <button className="border border-blueDeep text-blueDeep text-center py-3 px-5 sm:px-10 text-lg rounded-lg mb-3 w-full md:w-[360px] hover:bg-blueDeep hover:text-black hover:scale-90">
+                  Finance Tracking
+                </button>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+      <section className="left-64 h-full bg-gray-200 dark:bg-gray-800"></section>
     </>
   );
 }

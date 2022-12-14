1902352cac0cf4eed5195dc52edc193631d04326
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2022-12-14 15:59:41 +0100

    modified Dashboard.js in Pages

--- Client/src/Pages/Dashboard.js
+++ Client/src/Pages/Dashboard.js
@@ -11,10 +11,27 @@ function Dashboard() {
       <Sidebar />
       <section className="fixed h-screen left-64 display-size bg-gray-200 dark:bg-gray-800 p-9">
         <main>
-          <div className="row py-3 d-flex flex-row justify-between items-center">
+          <div className="w-full lg:w-2/3 py-3 flex justify-between items-center">
             <h2 className="font-raleway font-bold text-2xl">Dashboard</h2>
-            <i className="fas fa-search fa-lg fa-fw"></i>
-            <i className="fas fa-search fa-lg fa-fw"></i>
+            <div className="flex items-center justify-center space-x-5">
+              <i class="fas fa-bars fa-lg fa-fw"></i>
+              <p className="text-md font-raleway bg-gray-300 py-3 px-16 rounded-md">
+                <i className="fas fa-search fa-md fa-fw pr-3"></i>
+                Search
+              </p>
+            </div>
+          </div>
+          <div className="row w-full lg:w-2/3 py-4 flex space-x-6 overflow-x-auto">
+            <div className="p-10 bg-gray-300 dark:bg-gray-900 rounded-lg">
+              <i class="fas fa-bars fa-lg fa-fw"></i>
+              <span className="text-md font-light font-raleway">
+                Total Earning
+              </span>
+              <hr />
+              <p className="font-black font-archivo text-2xl">$450,000.30</p>
+            </div>
+            <div className="py-16 px-20 bg-gray-300 rounded-lg"></div>
+            <div className="py-16 px-20 bg-gray-300 rounded-lg"></div>
           </div>
         </main>
       </section>

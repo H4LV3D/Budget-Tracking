98bba4b104b846a37ba96fb32e3760254df77c92
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2022-12-15 08:57:27 +0100

    modified Navbar.js and Sidebar.js in Components

--- Client/src/Components/Navbar.js
+++ Client/src/Components/Navbar.js
@@ -26,34 +26,33 @@ const Navbar = () => {
     <>
       <div className="flex flex-row flex-wrap px-8 md:px-12 py-5 mx-auto md:items-center justify-between shadow-md lg:shadow-xl lg:rounded-xl lg:mx-10 text-gray-700 bg-gray-200 dark:bg-gray-800 md:pt-8">
         <div className="flex">
-          <a href="/" className="flex items-center md:mb-0 mr-3">
-            <i className="fab fa-bitcoin fa-lg fa-fw text-blueDeep"></i>
-            <span className="text-blueDeep font-sans font-bold text-lg">
-              Coin
-            </span>
-            <span className="text-gray-600 dark:text-gray-400 font-raleway font-medium text-lg">
-              Save
-            </span>
-          </a>
+          <Link to="/">
+            <p className="flex items-center md:mb-0 mr-3">
+              <i className="fab fa-bitcoin fa-lg fa-fw text-blueDeep"></i>
+              <span className="text-blueDeep font-sans font-bold text-lg">
+                Coin
+              </span>
+              <span className="text-gray-600 dark:text-gray-400 font-raleway font-medium text-lg">
+                Save
+              </span>
+            </p>
+          </Link>
           <nav className="hidden md:flex flex-wrap items-center justify-center ml-4 font-raleway">
-            <a
-              href="/about"
-              className="mr-5 text-sm font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200"
-            >
-              About Us
-            </a>
-            <a
-              href="/"
-              className="mr-5 text-sm font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200"
-            >
-              Budgets
-            </a>
-            <a
-              href="/"
-              className="mr-5 text-sm font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200"
-            >
-              Tracking
-            </a>
+            <Link to="/about">
+              <p className="mr-5 text-sm font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200">
+                About Us
+              </p>
+            </Link>
+            <Link to="/">
+              <p className="mr-5 text-sm font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200">
+                Budgets
+              </p>
+            </Link>
+            <Link to="/">
+              <p className="mr-5 text-sm font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200">
+                Tracking
+              </p>
+            </Link>
             <Link to="/dashboard">
               <p className="mr-5 text-sm font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200">
                 Dashboard
@@ -62,12 +61,14 @@ const Navbar = () => {
           </nav>
         </div>
         <div className="flex md:hidden items-center justify-center">
-          <button className="mx-3">
-            <i
-              className="fas fa-user-circle fa-fw fa-xl sm:px-5 py-3 text-blueDeep"
-              aria-hidden="true"
-            ></i>
-          </button>
+          <Link to="/login">
+            <button className="mx-3">
+              <i
+                className="fas fa-user-circle fa-fw fa-xl sm:px-5 py-3 text-blueDeep"
+                aria-hidden="true"
+              ></i>
+            </button>
+          </Link>
 
           <button className="mx-3" onClick={toggleMenu}>
             <i
@@ -81,41 +82,27 @@ const Navbar = () => {
           <Toggle />
           <div className="hidden lg:block ml-6">
             <nav className=" md:flex flex-wrap items-center justify-center text-base font-raleway">
-              <a
-                href="/login"
-                className="mr-3 md:mr-5 text-md font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200"
-              >
-                Sign In
-              </a>
-              <a
-                href="/register"
-                className="mr-3 md:mr-5 text-md font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200"
-              >
-                Register
-              </a>
+              <Link to="/login">
+                <p className="mr-3 md:mr-5 text-md font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200">
+                  Sign In
+                </p>
+              </Link>
+              <Link to="/register">
+                <p className="mr-3 md:mr-5 text-md font-semibold text-gray-700 dark:text-white rounded-xl hover:text-blueDeep hover:text-lg hover:font-black transition ease-in-out duration-200">
+                  Register
+                </p>
+              </Link>
             </nav>
-
-            <button className="sm:hidden inline-flex items-center p-2 rounded-md bg-blueDeep focus:outline-none shadow-lg">
-              <svg
-                className="w-5 h-5 text-white rounded-sm"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 6a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 6a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
           </div>
           <div className="lg:hidden">
-            <a href="/login" className="text-decoration-none">
-              <i
-                className="fas fa-user-circle fa-fw fa-xl px-sm-5 text-gray-600 pl-5"
-                aria-hidden="true"
-              ></i>
-            </a>
+            <Link to="/login">
+              <a href="/login" className="text-decoration-none">
+                <i
+                  className="fas fa-user-circle fa-fw fa-xl px-sm-5 text-gray-600 pl-5"
+                  aria-hidden="true"
+                ></i>
+              </a>
+            </Link>
           </div>
         </div>
       </div>
@@ -124,36 +111,36 @@ const Navbar = () => {
         className="hidden z-90 w-0 h-0 justify-center pt-12 bg-gray-900 opacity-0 duration-700 font-raleway font-medium text-lg"
       >
         <div className="flex flex-col text-white text-center text-xl font-lightName">
-          <a
-            className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300"
-            href="/"
-          >
-            About
-          </a>
-          <a
-            className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300"
-            href="/"
-          >
-            Projects
-          </a>
-          <a
-            className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300"
-            href="/"
-          >
-            Plans
-          </a>
-          <a
-            className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300"
-            href="/login"
-          >
-            Sign In
-          </a>
-          <a
-            className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300"
-            href="/register"
-          >
-            Register
-          </a>
+          <Link to="/about">
+            <p className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300">
+              About Us
+            </p>
+          </Link>
+          <Link to="/">
+            <p className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300">
+              Projects
+            </p>
+          </Link>
+          <Link to="/">
+            <p className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300">
+              Plans
+            </p>
+          </Link>
+          <Link to="/dashboard">
+            <p className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300">
+              Dashboard
+            </p>
+          </Link>
+          <Link to="/login">
+            <p className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300">
+              Sign In
+            </p>
+          </Link>
+          <Link to="/register">
+            <p className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300">
+              Register
+            </p>
+          </Link>
           <p
             className="hover:text-amber-500 my-6 hover:text-blueDeep hover:text-4xl transistion ease-in-out duration-300"
             href="/"
--- Client/src/Components/Sidebar.js
+++ Client/src/Components/Sidebar.js
@@ -4,6 +4,7 @@ import "../Assets/fontawesome-free-6.2.1-web/css/solid.css";
 import "../Assets/fontawesome-free-6.2.1-web/css/all.css";
 import "../Assets/fontawesome-free-6.2.1-web/css/brands.css";
 import "../Assets/fontawesome-free-6.2.1-web/css/fontawesome.css";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
@@ -14,126 +15,108 @@ function Sidebar() {
           aria-label="Sidenav"
         >
           <div className="overflow-y-auton overflow-x-hidden py-5 px-3 h-full bg-gray-200 border-r border-gray-300 dark:bg-gray-800 dark:border-gray-700">
-            <a
-              href="/"
-              className="flex items-center sm:ml-12 lg:ml-0 md:mb-0 mr-3 mt-4 px-2 py-3"
-            >
-              <i className="fab fa-bitcoin fa-lg fa-fw text-blueDeep"></i>
-              <span className="text-blueDeep font-sans font-bold text-lg">
-                Coin
-              </span>
-              <span className="text-gray-600 dark:text-gray-400 font-raleway font-black text-lg">
-                Save
-              </span>
-            </a>
+            <Link to="/">
+              <p className="flex items-center sm:ml-12 lg:ml-0 md:mb-0 mr-3 mt-4 px-2 py-3">
+                <i className="fab fa-bitcoin fa-lg fa-fw text-blueDeep"></i>
+                <span className="text-blueDeep font-sans font-bold text-lg">
+                  Coin
+                </span>
+                <span className="text-gray-600 dark:text-gray-400 font-raleway font-black text-lg">
+                  Save
+                </span>
+              </p>
+            </Link>
             <ul className="space-y-2 mt-5 font-raleway md:mb-14">
               <li>
-                <a
-                  href="/dashboard"
-                  className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group bg-gray-300 dark:bg-gray-700"
-                >
-                  <i className="fa-solid fa-house fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span className="ml-6">Dashboard</span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group bg-gray-300 dark:bg-gray-700">
+                    <i className="fa-solid fa-house fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                    <span className="ml-6">Dashboard</span>
+                  </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/"
-                  type="button"
-                  className="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  aria-controls="dropdown-pages"
-                  data-collapse-toggle="dropdown-pages"
-                >
-                  <i className="fas fa-file-invoice-dollar fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span className="flex-1 ml-6 text-left whitespace-nowrap">
-                    Invoice
-                  </span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    <i className="fas fa-file-invoice-dollar fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                    <span className="flex-1 ml-6 text-left whitespace-nowrap">
+                      Invoice
+                    </span>
+                  </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/"
-                  type="button"
-                  className="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  aria-controls="dropdown-sales"
-                  data-collapse-toggle="dropdown-sales"
-                >
-                  <i className="fas fa-cart-shopping fa-lg fa-fw dark:text-white text-blueDeep"></i>
-                  <span className="flex-1 ml-6 text-left whitespace-nowrap">
-                    Sales
-                  </span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    <i className="fas fa-cart-shopping fa-lg fa-fw dark:text-white text-blueDeep"></i>
+                    <span className="flex-1 ml-6 text-left whitespace-nowrap">
+                      Sales
+                    </span>
+                  </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/"
-                  className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                >
-                  <i className="fas fa-envelope fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span className="flex-1 ml-6 whitespace-nowrap">
-                    Messages
-                  </span>
-                  <span className="inline-flex justify-center items-center w-5 h-5 text-xs font-semibold rounded-full text-primary-800 bg-primary-100 dark:bg-primary-200 dark:text-primary-800">
-                    6
-                  </span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <i className="fas fa-envelope fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                    <span className="flex-1 ml-6 whitespace-nowrap">
+                      Messages
+                    </span>
+                    <span className="inline-flex justify-center items-center w-5 h-5 text-xs font-semibold rounded-full text-primary-800 bg-primary-100 dark:bg-primary-200 dark:text-primary-800">
+                      6
+                    </span>
+                  </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/"
-                  type="button"
-                  className="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  <i className="fas fa-lock fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span className="flex-1 ml-6 text-left whitespace-nowrap">
-                    Authentication
-                  </span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    <i className="fas fa-lock fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                    <span className="flex-1 ml-6 text-left whitespace-nowrap">
+                      Authentication
+                    </span>
+                  </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/"
-                  className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-                >
-                  <i className="fas fa-file fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span className="ml-6">Docs</span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                    <i className="fas fa-file fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                    <span className="ml-6">Docs</span>
+                  </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/"
-                  className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-                >
-                  <i className="fas fa-folder fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span className="ml-6">Components</span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                    <i className="fas fa-folder fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                    <span className="ml-6">Components</span>
+                  </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/"
-                  className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-                >
-                  <i className="fa-solid fa-circle-info fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span className="ml-6">Help</span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                    <i className="fa-solid fa-circle-info fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                    <span className="ml-6">Help</span>
+                  </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/"
-                  className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-                >
-                  <i className="fas fa-gear fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span className="ml-6">Settings</span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                    <i className="fas fa-gear fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                    <span className="ml-6">Settings</span>
+                  </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/"
-                  className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-                >
-                  <i className="fas fa-globe-africa fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
-                  <span className="ml-6">Language</span>
-                </a>
+                <Link to="/dashboard">
+                  <p className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                    <i className="fas fa-globe-africa fa-lg fa-fw dark:text-white text-blueDeep my-auto"></i>
+                    <span className="ml-6">Language</span>
+                  </p>
+                </Link>
               </li>
               <li>
                 <p className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
@@ -151,12 +134,11 @@ function Sidebar() {
                   Kinkunmz
                 </small>
               </div>
-              <a
-                href="/login"
-                className=" text-gray-500 hover:text-gray-900 p-3 cursor-pointer dark:text-gray-400 dark:hover:text-blueDeep "
-              >
-                <i className="fas fa-sign-out-alt fa-lg fa-fw  py-auto"></i>
-              </a>
+              <Link to="/login">
+                <p className=" text-gray-500 hover:text-gray-900 p-3 cursor-pointer dark:text-gray-400 dark:hover:text-blueDeep ">
+                  <i className="fas fa-sign-out-alt fa-lg fa-fw py-auto"></i>
+                </p>
+              </Link>
             </div>
           </div>
         </aside>
